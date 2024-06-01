538d6bd8e21847518344ef2cf16bece843772ae3
Umme Habiba <125300455+payel2904@users.noreply.github.com>
2024-06-01 12:55:43 +0600

    feat: connect post table with category

--- resources/views/backend/category/show.blade.php
+++ resources/views/backend/category/show.blade.php
@@ -2,12 +2,36 @@
 @section('main_content')
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Category View</h1>
+        <h1 class="h3 mb-0 text-gray-800">{{ $category->name }}</h1>
     </div>
 
     <div class="card">
         <div class="card--body">
-            <h1>{{ $category->name }}</h1>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Desc</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Feature Image</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($category->posts as $post)
+                    <tr>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->description }}</td>
+                        <td>{{ $post->category->name }}</td>
+                        <td>
+                            @if($post->feature_image)
+                                <img src="{{ asset('storage/'.$post->feature_image) }}" alt="title" width="50px"
+                                     height="50px">
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 
